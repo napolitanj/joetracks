13ab7c29ee1b69f54e25cdd21b61b9facb2e9d31
JN <napolitanjoe@gmail.com>
2025-04-07 14:51:01 -0600

    add navbar, animations, formatting for general page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
 import "/src/styles/App.css";
 import BannerImage from "./components/BannerImage";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <BannerImage></BannerImage>
+      <Navbar></Navbar>
     </>
   );
 }
