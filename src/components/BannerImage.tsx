b22d40e18d291c56b510e7ce04ff4dd7ca438917
JN <91563434+napolitanj@users.noreply.github.com>
2025-04-07 23:57:27 -0600

    style edits for navbar and general formatting

--- src/components/BannerImage.tsx
+++ src/components/BannerImage.tsx
@@ -14,19 +14,21 @@ const BannerImage = () => {
   return (
     <>
       <div className="banner">
-        <div className="banner-container"></div>
-        <img src={headerPhoto} className="banner-image" alt="man skiing"></img>
-        <div
-          className="banner-text"
-          style={{ transform: `translateY(${offset}px)` }}
-        >
-          <h1>Joe Napolitan</h1>
-          <div className="subheader">
-            <div>
+        <div className="banner-container">
+          <img
+            src={headerPhoto}
+            className="banner-image"
+            alt="man skiing"
+          ></img>
+          <div
+            className="banner-text"
+            style={{ transform: `translateY(${offset}px)` }}
+          >
+            <h1>Joe Napolitan</h1>
+            <div className="subheader">
               <p>Information Technology</p>
               <p>Front-End Engineering</p>
               <p>Outdoor Recreation</p>
-              <p>Gaming</p>
             </div>
           </div>
         </div>
