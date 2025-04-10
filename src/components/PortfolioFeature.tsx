25353844b49b4a90fb9b7b3ec3a45ce26f076f6d
JN <napolitanjoe@gmail.com>
2025-04-10 14:21:35 -0600

    Add banner styles, route container styles, general formatting for portfolio route.

---
+++ src/components/PortfolioFeature.tsx
@@ -0,0 +1,27 @@
+import "../styles/PortfolioFeature.css";
+
+const PortfolioFeature = () => {
+  return (
+    <>
+      <div className="feature-container">
+        <h2 className="feature-title">Portfolio Feature</h2>
+        <img
+          src="src/assets/images/howtousecaltopo.png"
+          className="feature-image"
+        ></img>
+        <p className="feature-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <a href="">Feature Link</a>
+      </div>
+    </>
+  );
+};
+
+export default PortfolioFeature;
