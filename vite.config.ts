a2966f54b37028b0781fae63ca318694e57af220
napolitanj <91563434+napolitanj@users.noreply.github.com>
2025-10-17 11:00:24 -0400

    updated config and package files for deployments

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/joetracks/",
   build: {
-    outDir: "dist",
+    outDir: "docs",
   },
   plugins: [react()],
   server: {
