1b1d409ffc3f85a49016face6cca9780cf1fd4f9
napolitanj <91563434+napolitanj@users.noreply.github.com>
2025-10-20 10:26:19 -0400

    Fix Cloudflare Pages build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/",
   build: {
-    outDir: "docs",
+    outDir: "dist",
   },
   plugins: [react()],
   server: {
