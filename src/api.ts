6eaf59642a0c3cfc8165a094ce213211bd98de54
napolitanj <91563434+napolitanj@users.noreply.github.com>
2025-10-24 13:23:36 -0400

    add token-saving line to login()

--- src/api.ts
+++ src/api.ts
@@ -14,6 +14,7 @@ export async function login(password: string) {
   }
 
   const data = await res.json();
+  localStorage.setItem("authToken", data.token);
 
   if (data.token) {
     localStorage.setItem("auth_token", data.token);
